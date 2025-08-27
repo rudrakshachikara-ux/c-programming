4830fe44144f7464c00c02d02682d7f4b4c3ce9e
rudrakshachikara-ux <rudrakshachikara@gmail.com>
2025-08-27 20:04:53 +0530

    Add files via upload

---
+++ all (15).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    int x,y;
+    printf("Enter the number \n");
+    scanf("%d", &x);
+    printf("Enter another number \n");
+    scanf("%d", &y);
+    printf("%d \n",x>=y);
+    printf("%d \n",x<=y);
+    printf("%d \n",x>y);
+    printf("%d \n",x<y);
+    printf("%d \n",x==y);
+    printf("%d \n",x!=y);
+    return 0;
+}
+
